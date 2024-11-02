b0075bbe2cf63c1ba0e6f4790c315f7fe0b86da0
RiTiK <168961238+ritikkumar8z@users.noreply.github.com>
2024-11-02 08:46:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function createFirework() {
+    const firework = document.createElement("div");
+    firework.classList.add("firework");
+
+    // Random position and size
+    firework.style.left = Math.random() * 100 + "vw";
+    firework.style.top = Math.random() * 100 + "vh";
+    firework.style.animationDuration = (Math.random() * 2 + 1) + "s";
+
+    document.body.appendChild(firework);
+
+    // Explosion effect
+    setTimeout(() => {
+      firework.style.width = "15px";
+      firework.style.height = "15px";
+      firework.style.animation = "burst 0.5s ease-out forwards";
+    }, 1500);
+
+    // Remove firework
+    setTimeout(() => {
+      firework.remove();
+    }, 2000);
+}
+
+// Create fireworks every 300ms
+setInterval(createFirework, 300);
